c37a3547f1e170b609078d936fb90cfa92e67642
Fernando Figueroa Fuentes <fer.figueroaf@alumnos.duoc.cl>
2020-10-19 12:14:05 -0300

    Creamos primera clase

--- locallibreria/catalogo/views.py
+++ locallibreria/catalogo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import  Cliente
+from django.views import generic
 
 # Create your views here.
+def index(request):
+    num_clientes = Cliente.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={ 'num_clientes': num_clientes},
+    )
